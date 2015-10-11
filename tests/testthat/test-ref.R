7906c6a99ba75455f1543b59df10f8d9e18727e8
Kun Ren <renkun@outlook.com>
2015-10-11 09:14:13 +0800

    add test cases

--- tests/testthat/test-ref.R
+++ tests/testthat/test-ref.R
@@ -12,3 +12,21 @@ test_that("deref", {
   after <- deref(r1)
   expect_identical(before, after)
 })
+
+test_that("constraint", {
+  r1 <- ref(1:10, is.integer)
+  expect_identical(constraint(r1), is.integer)
+  r1[1] <- 0L
+  expect_identical(deref(r1), c(0L, 2:10))
+  expect_error(r1[2] <- "a")
+  expect_error(update(r1, "abc"))
+  
+  r2 <- ref(rnorm(100), function(x) is.numeric(x) && length(x) >= 100)
+  new_data <- rnorm(200)
+  expect_identical({
+    update(r2, new_data)
+    deref(r2)
+  }, new_data)
+  expect_error(r2[1] <- "a")
+  expect_error(update(r2, rnorm(50)))
+})
